ad6bf8c7e6be85a4c465b8ef0bd019c4831adf24
edcilo <edcilop@gmail.com>
2020-09-10 16:15:57 -0500

    add sql tables

---
+++ psql/stdesktop_servers.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS stdesktop_servers (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    test_id BIGINT NULL,
+    server_id INT NULL,
+    download_connections SMALLINT NULL,
+    download_bytes BIGINT NULL,
+    download_bytes_percent FLOAT NULL,
+    name VARCHAR(120) NULL,
+    country VARCHAR(120) NULL,
+    country_code VARCHAR(10) NULL,
+    latitude FLOAT NULL,
+    longitude FLOAT NULL,
+    sponsor_name VARCHAR(120) NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
